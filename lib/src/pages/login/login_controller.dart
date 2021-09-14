f1e382bfc7d7df9db7929b940ed2e2ed787f5e91
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-09-14 10:34:40 -0500

    feat: Set logged user in sesion

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -1,7 +1,7 @@
-import 'dart:ffi';
-
+import 'package:ex_t1_app/src/models/user.dart';
 import 'package:ex_t1_app/src/providers/auth_provider.dart';
 import 'package:ex_t1_app/src/utils/shared_pref.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ndialog/ndialog.dart';
@@ -15,12 +15,14 @@ class LoginController {
 
   late AuthProvider _authProvider;
   late SharedPref _sharedPref;
+  late final userRef;
   String? _typeUser;
 
   Future init(BuildContext context) async {
     this.context = context;
     _authProvider = new AuthProvider();
     _sharedPref = new SharedPref();
+    userRef = FirebaseDatabase.instance.reference();
     _typeUser = await _sharedPref.read('typeUser');
     print(_typeUser);
   }
@@ -51,11 +53,67 @@ class LoginController {
 
       if (isLogin) {
         if (_typeUser == 'seller') {
-          Navigator.pushNamedAndRemoveUntil(
-              context, 'seller/home', (route) => false);
+          bool flag = false;
+          Query _compradorQuery = userRef
+              .child('vendedores')
+              .orderByChild("email")
+              .equalTo(emailController.text);
+
+          _compradorQuery.get().then((value) {
+            Map<dynamic, dynamic> map = value.value;
+            map.forEach((key, value) {
+              if (emailController.text == value['email']) {
+                flag = true;
+                User user = User(
+                    email: value['email'],
+                    username: value['username'],
+                    name: value['name'],
+                    lastname: value['lastname'],
+                    id: _authProvider.getUser().uid);
+                _sharedPref.save('user', user.toJson());
+
+                Navigator.pushNamedAndRemoveUntil(
+                    context, 'seller/home', (route) => false);
+              }
+            });
+
+            if (!flag) {
+              final snackBar = SnackBar(
+                  content: Text(
+                      'El usuario no se encuentra registrado como vendedor'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          });
         } else if (_typeUser == 'client') {
-          Navigator.pushNamedAndRemoveUntil(
-              context, 'client/home', (route) => false);
+          bool flag = false;
+          Query _compradorQuery = userRef
+              .child('clientes')
+              .orderByChild("email")
+              .equalTo(emailController.text);
+
+          _compradorQuery.get().then((value) {
+            Map<dynamic, dynamic> map = value.value;
+            map.forEach((key, value) {
+              if (emailController.text == value['email']) {
+                print(value);
+                User user = User(
+                    email: value['email'],
+                    username: value['username'],
+                    name: value['name'],
+                    lastname: value['lastname'],
+                    id: _authProvider.getUser().uid);
+                _sharedPref.save('user', user.toJson());
+                Navigator.pushNamedAndRemoveUntil(
+                    context, 'client/home', (route) => false);
+              }
+            });
+            if (!flag) {
+              final snackBar = SnackBar(
+                  content: Text(
+                      'El usuario no se encuentra registrado como vendedor'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          });
         }
         print('El usuario esta logeado');
       }
